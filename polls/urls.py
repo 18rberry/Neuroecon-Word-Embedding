144f5b2878d069492e7cf9e4506656a3d3d6bf41
Chris Wong <chriskw.xyz@gmail.com>
2019-02-21 15:30:17 -0800

    Removed polls path, renders not on root

--- polls/urls.py
+++ polls/urls.py
@@ -3,18 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index', views.index, name='index'),
+    path('', views.index, name='index'),
     path('reasoning', views.reasoning, name='reasoning'),
     path('graph', views.graph, name='graph'),
-    path('result', views.result, name='result')
-    # path('reasoningExtended', views.reasoningExtended, name='reasoningExtended'),
-    # path('stereotype', views.stereotype, name='stereotype')
 ]
-
-#when calling graph: uses GET method
-#graph_form
-#path('graph_form', views.graph_form, name = 'graph_form')
-
-#http verbs:
-#GET: retrieve remote data
-#POST: create new data
