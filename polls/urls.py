e88c7e0e0f02eaeea7d35ea44246934450ef806c
Chris Wong <chriskw.xyz@gmail.com>
2019-02-22 02:52:25 -0800

    Added /api/graph endpoint

--- polls/urls.py
+++ polls/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('reasoning', views.reasoning, name='reasoning'),
     path('graph', views.graph, name='graph'),
+    path('api/graph', views.graph_api, name='graph_api')
 ]
